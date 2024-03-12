9309ed2693a034931982a68d565f15c69bad5b50
gunwoo <gunwoo45954@gmail.com>
2024-03-12 09:41:13 +0900

    preprocess poem_dataset

--- ML/AI_Poet/train.py
+++ ML/AI_Poet/train.py
@@ -1,9 +1,7 @@
 from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, DataCollatorWithPadding
-from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import pandas as pd
 import torch
-import re
 
 from transformers import TrainingArguments, Trainer
 
@@ -32,35 +30,7 @@ class Poem_Dataet(Dataset):
             "attention_mask": attention_mask,
             "label_ids": label_ids
         }
-    
-def data_preprocessing(df):
 
-    # NaN 제거
-    dataset = df.dropna(subset=["poem"]) # 현재 poem쪽에 NaN이 존재해서 처리해줌. TODO : 크롤링할 때, poem==NaN이면 skip하도록 구현
-
-    # 중복 제거
-    dataset = dataset.drop_duplicates(["poem"], keep="first")
-
-    # 1000자 이상의 데이터 제거
-    dataset = dataset[dataset["poem"].str.len() <= 1000]
-
-    # index 재설정
-    dataset = dataset.reset_index(drop=True) 
-
-    # 아래의 패턴들을 전처리
-    # 1. "\xa0"
-    # 2. "\s" * m (m > 1)
-    # 3. "\n\n "
-    # 4. "\n" * m (m > 2)
-    for i in range(len(dataset)):
-        dataset.iloc[i, 2] = dataset.iloc[i, 2].replace(u"\xa0", u"")
-        dataset.iloc[i, 2] = re.sub(r"( {2,})", "", dataset["poem"][i])
-        dataset.iloc[i, 2] = re.sub(r"\n\n ", "", dataset["poem"][i])
-        dataset.iloc[i, 2] = re.sub(r"(\n{3,})", "", dataset["poem"][i])
-
-    return dataset
-
-    
 
 if __name__ == "__main__":
     # device 정의
@@ -72,20 +42,19 @@ if __name__ == "__main__":
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     # train dataset load & preprocessing
-    file_path = "my_dataset/crawling_poem.csv"
+    file_path = "dataset/poem_dataset.csv"
 
     dataset = pd.read_csv(file_path)
-    processed_dataset = data_preprocessing(dataset)
-    train_dataset = list(processed_dataset["poem"])
+    train_dataset = list(dataset["poem"])
     train_data = Poem_Dataet(train_dataset, tokenizer)
 
     # set TrainingArguments
     training_args=TrainingArguments(
-    output_dir="output/",
+    output_dir="output/version_2",
     overwrite_output_dir=True,
     logging_steps=500,
     save_steps=500,
-    save_total_limit=2,
+    save_total_limit=1,
     learning_rate= 1e-05,
     per_device_train_batch_size=2,
     num_train_epochs=10,
