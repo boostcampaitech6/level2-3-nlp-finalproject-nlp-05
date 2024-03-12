b09065a15184f693f268d9a431b8dfa57efe8a02
rlarlgh96 <rlarlgh96@khu.ac.kr>
2024-03-12 04:18:16 +0000

    Update app.py and index.html

--- ML/Metaphor_Gen/code/train.py
+++ ML/Metaphor_Gen/code/train.py
@@ -516,16 +516,16 @@ def main():
     #     return result
 
     # Initialize our Trainer
-    training_args.num_train_epochs = 10
+    training_args.num_train_epochs = 30
     training_args.save_strategy = 'no'
     trainer = Seq2SeqTrainer(
         model=model,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
-        eval_dataset=eval_dataset if training_args.do_eval else None,
+        # eval_dataset=eval_dataset if training_args.do_eval else None,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        compute_metrics=compute_metrics if training_args.predict_with_generate else None,
+        # compute_metrics=compute_metrics if training_args.predict_with_generate else None,
     )
 
     # Training
