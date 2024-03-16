0d134f0f1dbdd55c7c6e284a413aea2b946e7672
JaehyeokSim <116555114+simson0524@users.noreply.github.com>
2024-03-16 13:56:58 +0900

    Update schemas.py

--- Application/schemas.py
+++ Application/schemas.py
@@ -2,7 +2,10 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class LineRequest(BaseModel):
-    emotion: Optional[str] = None
+    emotion: str
+
+class LineResponse(BaseModel):
+    
 
 class PoemRequest(BaseModel):
-    line: Optional[str] = None
+    line: str
