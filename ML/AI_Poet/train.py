c2f99831a24cbf1e7e8a87acb635a2604929941d
gunwoo <gunwoo45954@gmail.com>
2024-03-24 05:41:17 +0000

    시 생성 모델 code refactoring

--- ML/AI_Poet/train.py
+++ ML/AI_Poet/train.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorWithPadding
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, DataCollatorWithPadding
 from torch.utils.data import Dataset
 import pandas as pd
 import torch
@@ -15,7 +15,8 @@ class Poem_Dataet(Dataset):
 
         # tokenize dataset
         for data in self.dataset:
-            tokenized_data = self.tokenizer(data, add_special_tokens=True, max_length=512, padding=False, truncation=True, return_tensors=None, return_token_type_ids=False)
+            data = "<s>" + data + "</s>"
+            tokenized_data = self.tokenizer(data, add_special_tokens=True, max_length=512, padding="max_length", truncation=True, return_tensors="pt")
             self.tokenized_dataset.append(tokenized_data)
 
     def __len__(self):
@@ -23,8 +24,14 @@ class Poem_Dataet(Dataset):
 
     def __getitem__(self, index):
         item = self.tokenized_dataset[index]
-        item["labels"] = item["input_ids"]
-        return item
+        input_ids = item["input_ids"].squeeze(0)
+        label_ids = item["input_ids"].squeeze(0)
+        attention_mask = item["attention_mask"].squeeze(0)
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "label_ids": label_ids
+        }
 
 
 if __name__ == "__main__":
@@ -32,31 +39,15 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # load model, tokenizer 
-    model = AutoModelForCausalLM.from_pretrained('EleutherAI/polyglot-ko-3.8b').to(device)
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/polyglot-ko-3.8b")
-    tokenizer.pad_token = tokenizer.eos_token
-    
+    model = GPT2LMHeadModel.from_pretrained('skt/ko-gpt-trinity-1.2B-v0.5').to(device)
+    tokenizer = PreTrainedTokenizerFast.from_pretrained('skt/ko-gpt-trinity-1.2B-v0.5',
+        bos_token='<s>', eos_token='</s>', unk_token='<unk>', pad_token='<pad>', mask_token='<mask>')
+
     # add special token
-    tokenizer.add_special_tokens({'additional_special_tokens': ["[YUN]"]})
+    tokenizer.add_special_tokens({'additional_special_tokens': ["<yun>"]})
     model.resize_token_embeddings(len(tokenizer))
 
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt", padding=True)
-
-    # freeze layers
-    for parameter in model.parameters():
-        parameter.requires_grad = False
-
-    for i, m in enumerate(model.gpt_neox.layers):
-        # Only un-freeze the last n transformer blocks
-        if i >= 24:
-            for parameter in m.parameters():
-                parameter.requires_grad = True
-
-    for parameter in model.gpt_neox.final_layer_norm.parameters():
-        parameter.requires_grad = True
-
-    for parameter in model.embed_out.parameters():
-        parameter.requires_grad = True
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     # train dataset load & preprocessing
     file_path = "dataset/poem_dataset.csv"
@@ -64,25 +55,20 @@ if __name__ == "__main__":
     dataset = pd.read_csv(file_path)
     dataset = dataset[dataset["num_lines"] > 5]
     train_dataset = list(dataset["poem"])
-
-    for i in range(len(train_dataset)):
-        train_dataset[i] = train_dataset[i].replace("<yun>", "[YUN]")
-
     train_data = Poem_Dataet(train_dataset, tokenizer)
 
     # set TrainingArguments
     training_args=TrainingArguments(
         output_dir="output/",
         overwrite_output_dir=True,
-        logging_steps=2000,
-        save_steps=2000,
+        logging_steps=1000,
+        save_steps=1000,
         save_total_limit=1,
-        learning_rate= 2e-05,
-        per_device_train_batch_size=1,
-        gradient_accumulation_steps=8,
-        num_train_epochs=15,
+        learning_rate= 1e-05,
+        per_device_train_batch_size=4,
+        num_train_epochs=10,
         lr_scheduler_type="linear",
-        warmup_ratio=0.1,
+        warmup_steps=1000,
         seed=42,
         fp16=True
     )
